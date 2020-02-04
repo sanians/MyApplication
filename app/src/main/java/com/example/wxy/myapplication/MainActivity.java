45dbd62dfd53f9f04838aad439157d76ebc59c76
sanians <1025912929@qq.com>
2020-02-04 22:07:16 +0800

    测试提交

--- app/src/main/java/com/example/wxy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wxy/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        //提交测试内容
 
 
         setContentView(R.layout.activity_main);
